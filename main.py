7b05c125e5ec6afc99aacb93be329c90dd3d6699
marcelohz <marcelohz@gmail.com>
2017-08-13 00:15:50 -0300

    Second commit

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-from flask import Flask, render_template, request, redirect
+import os
 import psycopg2
+from flask import Flask, render_template, request, redirect, flash, url_for
+from werkzeug.utils import secure_filename
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
+UPLOAD_FOLDER = app.root_path + '/static/pix/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = os.urandom(16)
+
 
 conn = psycopg2.connect("host=localhost dbname=intranet password=master user=postgres")
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/foto_upload', methods=['GET', 'POST'])
+def foto_upload():
+    if request.method == 'POST':
+        album_id = str(request.form.get('album_id'))
+        file = request.files['upload']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            cur = conn.cursor()
+            cur.execute("insert into intranet.foto (album_id, arquivo) values (%s, %s);", (album_id, filename))
+            conn.commit()
+            cur.close()
+            return redirect(url_for('fotos', id=album_id))
+
 
 
 @app.route('/albuns')
@@ -35,7 +66,7 @@ def fotos(id):
     fotos = cur.fetchall()
     cur.close()
 
-    return render_template('fotos.html', fotos = fotos)
+    return render_template('fotos.html', fotos = fotos, album_id = id)
 
 if __name__ == '__main__':
     app.run()
