e8fd74e9c8e3861d3fe1fdae8c82b3da90151885
marcelohz <marcelohz@gmail.com>
2017-08-15 09:37:29 -0300

    Second commit

--- main.py
+++ main.py
@@ -28,10 +28,14 @@ def favicon():
 
 
 @app.route('/')
-def hello_world():
-    session['usuario'] = None
+def index():
     return render_template('index.html')
 
+
+@app.route('/logout')
+def logout():
+    session['usuario'] = None
+    return redirect('/')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -45,6 +49,7 @@ def login():
         cur = conn.cursor()
         cur.execute("SELECT intranet.autentica(%s, %s, %s, %s);", (user, password, ip, hostname))
         result = cur.fetchone()
+        conn.commit() # commitamos o insert que existe na função autentica
         cur.close()
         if result[0] is True:
             session['usuario'] = user
@@ -64,7 +69,7 @@ def foto_upload():
             filename = secrets.token_hex(8) + "." + filename.rsplit('.', 1)[1].lower()
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             cur = conn.cursor()
-            cur.execute("insert into intranet.foto (album_id, arquivo) values (%s, %s);", (album_id, filename))
+            cur.execute('INSERT INTO intranet.foto (album_id, arquivo) VALUES (%s, %s);', (album_id, filename))
             conn.commit()
             cur.close()
         return redirect(url_for('fotos', id=album_id))
@@ -83,7 +88,7 @@ def albuns():
 def add_album():
     album = str(request.form.get('album'))
     cur = conn.cursor()
-    cur.execute("insert into intranet.album(nome) values(%s);", (album,))
+    cur.execute("INSERT INTO intranet.album(nome) VALUES(%s);", (album,))
     conn.commit()
     cur.close()
     return redirect("/albuns")
@@ -92,7 +97,9 @@ def add_album():
 @app.route('/fotos/<id>')
 def fotos(id):
     cur = conn.cursor()
-    cur.execute('SELECT arquivo, nome FROM intranet.foto, intranet.album where foto.album_id = album.id and album_id = %s;', (id,))
+    cur.execute(
+        'SELECT arquivo, nome FROM intranet.foto, intranet.album WHERE foto.album_id = album.id AND album_id = %s;',
+        (id,))
     fotos = cur.fetchall()
     cur.close()
     return render_template('fotos.html', fotos=fotos, album_id=id)
@@ -102,44 +109,53 @@ def fotos(id):
 def del_foto(arquivo):
     if session['usuario'] is None:
         return redirect(404)
-    sql = 'delete from intranet.foto where arquivo = %s'
+
+    # primeiro pegamos o album pra saber pra onde voltar
+    sql = 'SELECT album_id FROM intranet.album, intranet.foto WHERE album.id = foto.album_id AND foto.arquivo = %s'
     cur = conn.cursor()
+    cur.execute(sql, (arquivo,))
+    album_id = str(cur.fetchone()[0])
+
+    sql = 'DELETE FROM intranet.foto WHERE arquivo = %s'
     cur.execute(sql, (arquivo,))
     conn.commit()
     cur.close()
     os.remove(app.root_path + '/static/pix/' + arquivo)
+    return redirect('/fotos/' + album_id)
 
 
 @app.route('/busca_ramal', methods=['POST'])
 def busca_ramal():
     sql = '''
-      SELECT distinct setor.nome, string_agg(distinct funcionario.nome, ', '), string_agg(distinct numero, ', ') from intranet.setor, intranet.funcionario, intranet.ramal
-          where funcionario.setor_id = setor.id and ramal.setor_id = setor.id and funcionario.nome ilike %s group by setor.nome
+      SELECT DISTINCT setor.nome, string_agg(DISTINCT funcionario.nome, ', '), string_agg(DISTINCT numero, ', ') 
+        FROM intranet.setor, intranet.funcionario, intranet.ramal
+        WHERE funcionario.setor_id = setor.id AND ramal.setor_id = setor.id AND funcionario.nome ILIKE %s 
+        GROUP BY setor.nome
     '''
     query = request.form.get('query')
     cur = conn.cursor()
-    cur.execute(sql, ('%' + query +'%',))
+    cur.execute(sql, ('%' + query + '%',))
     ramais = cur.fetchall()
     cur.close()
-    return render_template('busca_ramal.html', ramais = ramais)
+    return render_template('busca_ramal.html', ramais=ramais)
 
 
 @app.route('/add_ramal', methods=['POST'])
 def add_ramal():
     cur = conn.cursor()
-    sql = 'SELECT setor.id from intranet.setor where nome = upper(%s)'
+    sql = 'SELECT setor.id FROM intranet.setor WHERE nome = upper(%s)'
     cur.execute(sql, (request.form.get('setor'),))
     setor_id = cur.fetchone()
     if setor_id is None:
         flash('Setor inexistente.', 'error')
         return redirect("/ramais")
 
-    sql = 'insert into intranet.ramal (numero, setor_id) values (%s, %s) RETURNING *;'
+    sql = 'INSERT INTO intranet.ramal (numero, setor_id) VALUES (%s, %s) RETURNING *;'
     cur.execute(sql, (request.form.get("numero"), setor_id))
     conn.commit()
     test = cur.fetchone()
     cur.close()
-    if test != None:
+    if test is not None:
         flash('Ramal adicionado.', 'info')
     return redirect("/ramais")
 
@@ -148,15 +164,15 @@ def add_ramal():
 def ramais():
     cur = conn.cursor()
 
-    sql = '''SELECT setor.nome, numero from intranet.setor, intranet.ramal
-          where ramal.setor_id = setor.id
-          order by setor.nome
+    sql = '''SELECT setor.nome, numero FROM intranet.setor, intranet.ramal
+          WHERE ramal.setor_id = setor.id
+          ORDER BY setor.nome
         '''
     cur.execute(sql)
     ramais = cur.fetchall()
     cur.close()
 
-    return render_template('ramais.html', ramais = ramais)
+    return render_template('ramais.html', ramais=ramais)
 
 
 @app.route('/del_func/<id>')
@@ -164,7 +180,7 @@ def del_func(id):
     if session['usuario'] is None:
         return redirect(404)
     cur = conn.cursor()
-    cur.execute('delete from intranet.funcionario where id = %s;', (id,))
+    cur.execute('DELETE FROM intranet.funcionario WHERE id = %s;', (id,))
     conn.commit()
     flash('Funcionário excluído', 'info')
     return redirect('/funcionarios')
@@ -172,20 +188,22 @@ def del_func(id):
 
 @app.route('/funcionarios', methods=['GET', 'POST'])
 def funcionarios():
-    sql = "select id, nome from intranet.setor order by nome"
+    sql = "SELECT id, nome FROM intranet.setor ORDER BY nome"
     cur = conn.cursor()
     cur.execute(sql)
     setores = cur.fetchall()
     cur.close()
 
-    sql = 'select funcionario.id, funcionario.nome, setor.id, setor.nome from intranet.setor, intranet.funcionario where funcionario.setor_id = setor.id'
+    sql = '''SELECT funcionario.id, funcionario.nome, setor.id, setor.nome FROM intranet.setor, 
+            intranet.funcionario WHERE funcionario.setor_id = setor.id
+          '''
     cur = conn.cursor()
     cur.execute(sql)
     funcsets = [dict(func_id=r[0], func_nome=r[1], setor_id=r[2], setor_nome=r[3]) for r in cur.fetchall()]
     cur.close()
 
     if request.method == 'POST':
-        sql = 'insert into intranet.funcionario(nome, setor_id) values(%s, %s)'
+        sql = 'INSERT INTO intranet.funcionario(nome, setor_id) VALUES(%s, %s)'
         cur = conn.cursor()
         cur.execute(sql, (request.form.get('nome'), request.form.get('setor_id')))
         conn.commit()
